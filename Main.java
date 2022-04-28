6c9b21372fdf33c4d1e7f23cbbcd3078affb01fb
sintof <103381274+sintof@users.noreply.github.com>
2022-04-28 16:32:56 +0530

    Upgradede Main.java
    added functions input and inputSidesOfTriangle

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+package Triangle;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
-        Triangle triangle = new Triangle(1,2,3);
+        Triangle triangle = new Triangle(inputSidesOfTriangle());
+    }
+    public static int[] inputSidesOfTriangle(){
+        int sideA = input("Введите сторону A");
+        int sideB = input("Введите сторону B");
+        int sideC = input("Введите сторону C");
+        return new int[]{sideA,sideB,sideC};
+    }
+    private static int input(String comment){
+        System.out.printf("%s >> ", comment);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+    private static int input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
